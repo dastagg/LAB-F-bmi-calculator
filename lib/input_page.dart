138a3949c302141945157391a25f19c3cbe076ea
dastagg:opus5 <dastagg@stagglinux.com>
2022-02-19 04:45:22 -0500

    Section 12-2 Completed.

---
+++ lib/input_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  const InputPage({Key? key}) : super(key: key);
+
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('BMI CALCULATOR')),
+        backgroundColor: const Color(0xFF0A0E21),
+      ),
+      body: const Center(
+        child: Text('Body Text'),
+      ),
+    );
+  }
+}
